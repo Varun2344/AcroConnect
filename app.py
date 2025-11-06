39d9fe0a2c61642b59a4206bbf7ab2a8665a64e2
Varun Purohit <varunpurohit2344@gmail.com>
2025-11-06 10:04:09 +0530

    Gemini version update

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if not api_key:
 else:
     try:
         genai.configure(api_key=api_key)
-        model = genai.GenerativeModel('gemini-1.0-pro')
+        model = genai.GenerativeModel('gemini-1.5-pro-latest')
     except Exception as e:
         st.error(f"Error configuring Gemini API: {e}")
 
